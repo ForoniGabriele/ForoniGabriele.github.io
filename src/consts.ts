32a859dd6a8e7c771eb06f1924f4f4387af6b5de
gabriele <gabriele.foroni@webranking.it>
2026-08-24 15:43:33 +0200

    inserimento progetti

---
+++ src/consts.ts
@@ -0,0 +1,3 @@
+// src/consts.ts
+export const SITE_TITLE = 'Gabriele Foroni — Portfolio';
+export const SITE_DESCRIPTION = 'Portfolio di Gabriele Foroni, sviluppatore web con passione per design e usabilità.';
